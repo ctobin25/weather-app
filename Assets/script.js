f0c8e1951beaf3457f0f81311cc9d6e9997a4fc1
caroline tobin <carolinetobin@carolines-MacBook-Pro.local>
2020-12-18 12:44:20 -0500

    created search buttom

--- Assets/script.js
+++ Assets/script.js
@@ -25,6 +25,17 @@ $(document).ready(function() {
 
       console.log(response);
 
+      // Constructing HTML containing the artist information
+    //   var artistName = $("<h1>").text(response.name);
+    //   var artistURL = $("<a>").attr("href", response.url).append(artistName);
+    //   var artistImage = $("<img>").attr("src", response.thumb_url);
+    //   var trackerCount = $("<h2>").text(response.tracker_count + " fans tracking this artist");
+    //   var upcomingEvents = $("<h2>").text(response.upcoming_event_count + " upcoming events");
+    //   var goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
+
+      // Empty the contents of the artist-div, append the new artist content
+    //   $("#artist-div").empty();
+    //   $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
     });
   }
    
